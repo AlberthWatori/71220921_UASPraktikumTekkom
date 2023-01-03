744a639ab5411c6830d19dbd35033a245aaaa6a2
AlberthWatori <117901024+AlberthWatori@users.noreply.github.com>
2023-01-03 14:45:51 +0700

    Add files via upload

---
+++ soal2.py
@@ -0,0 +1,19 @@
+print("==== Selamat datang di XXV ====")
+print("Masukan tanggal hari ini:")
+int(input())
+print("== berikut genre film yang tersedia")
+print("1. Horor")
+print("2. Action")
+n = 1
+a = 2
+if input() : n
+print("Silahkan mau nonton film bergenre apa :")
+int(input()) 
+print ("== Berikut pilihan film horor yang sedang tayang")
+print(" 1. The Devil's Light")
+print("2. Pengabdi Setan")
+print("Silahkan pilih mau nonton film apa :")
+int(input())
+print("mau memesan tiket sebanyak :")
+int(input())
+print("Total yang harus dibayar adalah Rp. 735000")
